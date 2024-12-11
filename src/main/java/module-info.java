a6458b31191b5c3784751422415ae6c7443d9fb3
Guilherme Cruz <guilhermegccruz@gmail.com>
2024-12-11 18:29:51 +0000

    Mudança nos Diretorios do projeto

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,16 @@
 module com.grupo6.projetoptda {
     requires javafx.controls;
     requires javafx.fxml;
-    requires java.sql;
     requires jbcrypt;
+    requires mysql.connector.j;
+    requires com.google.gson;
+    requires java.sql;
 
 
     opens com.grupo6.projetoptda to javafx.fxml;
+    opens com.grupo6.projetoptda.Controller to javafx.fxml;
+    opens com.grupo6.projetoptda.Getter to javafx.fxml;
     exports com.grupo6.projetoptda;
+    exports com.grupo6.projetoptda.Controller;
+    exports com.grupo6.projetoptda.Getter;
 }
