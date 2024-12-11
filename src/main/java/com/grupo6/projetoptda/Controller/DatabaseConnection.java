6a6901d304bddafed5783bdea2d341b4f0534bb9
Guilherme Cruz <guilhermegccruz@gmail.com>
2024-12-11 18:31:02 +0000

    Sim

--- src/main/java/com/grupo6/projetoptda/Controller/DatabaseConnection.java
+++ src/main/java/com/grupo6/projetoptda/Controller/DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/ptda24_bd_06";
     private static final String USER = "root";
-    private static final String PASSWORD = "VaiTeFoder123@@";
+    private static final String PASSWORD = "MUDEM A PASSWORD AQUI MAS NAO DEEM COMMIT A ESTE FICHEIRO";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
