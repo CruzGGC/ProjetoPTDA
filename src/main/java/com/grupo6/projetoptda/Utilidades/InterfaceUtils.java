687bd457bb39d4df12b72e0fe0efdd09a8062a8b
Alexandra <alexandradias05@ua.pt>
2024-12-13 21:32:20 +0000

    Adicionado Icones

---
+++ src/main/java/com/grupo6/projetoptda/Utilidades/InterfaceUtils.java
@@ -0,0 +1,23 @@
+package com.grupo6.projetoptda.Utilidades;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class InterfaceUtils {
+
+    public static void recarregarInterface() {
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(InterfaceUtils.class.getResource("/com/grupo6/projetoptda/GerirStockPanel.fxml")));
+            Scene scene = Stage.getWindows().get(0).getScene();
+            scene.setRoot(root);
+            CarregarCSS.applyCSS(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
